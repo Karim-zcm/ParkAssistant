d93dd710f4881884307c5c8a0f628b20ac5c74c2
Karim-zcm <karimmalik50@yahoo.de>
2023-02-04 13:58:29 +0100

    commit Datenbank test

--- app/src/main/java/de/haw/hamburg/parkassistant/Database.java
+++ app/src/main/java/de/haw/hamburg/parkassistant/Database.java
@@ -9,10 +9,12 @@ import androidx.annotation.Nullable;
 
 public class Database extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "user_database";
     private static final String TAG = "DBHelper";
     private static final String TABLE_NAME = "login_info";
     private static final String COL1 = "email";
     private static final String COL2 = "password";
+    private static final String COL3 = "username";
 
 
     public Database(Context context) {
@@ -21,7 +23,7 @@ public class Database extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (" + COL1 +" PRIMARY KEY, " + COL2 + " TEXT)";
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (" + COL1 +" PRIMARY KEY, " + COL2 + COL3 + " TEXT)";
         db.execSQL(createTable);
     }
 
@@ -29,13 +31,25 @@ public class Database extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
-
-
-        /*
-        public boolean addData(String item){
-            SQLiteDatabase db = this.getWritableDatabase();
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(COL1, COL2, item);
-        }*/
     }
+
+    public void addUser(String email, String password, String username){
+        System.out.println("here");
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        try{
+            ContentValues values = new ContentValues();
+            values.put(COL1, email);
+            values.put(COL2, password);
+            values.put(COL3, username);
+
+            db.insertOrThrow(DATABASE_NAME, null, values);
+            db.setTransactionSuccessful();
+        } catch (Exception e){
+            System.out.println("An error occurred while trying to add a user to the database");
+        } finally {
+            db.endTransaction();
+        }
+    }
+
 }
--- app/src/main/java/de/haw/hamburg/parkassistant/MainActivity.java
+++ app/src/main/java/de/haw/hamburg/parkassistant/MainActivity.java
@@ -10,8 +10,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView username;
     TextView email;
     TextView password;
+    TextView repeatPassword;
+
+  //  Database database;
 
 
     @Override
@@ -19,10 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        email = (TextView) findViewById(R.id.editTextTextEmailAddress);
+        username = (TextView) findViewById(R.id.editTextTextUsername);
         password = (TextView) findViewById(R.id.editTextTextPassword);
+        repeatPassword = (TextView) findViewById(R.id.editTextTextRepeatPassword);
+        email = (TextView) findViewById(R.id.editTextTextEmailAddress);
     }
 
+    // Login --------------------------
+    // Compare Login data
     public void loginButton(View view){
         if (email.getText().toString().equals("test@mail.de")) {
             Log.i("debugPrint", "correct Mail");
@@ -32,5 +40,16 @@ public class MainActivity extends AppCompatActivity {
             Log.i("debugPrint", "correct Password");
         } else Log.i("debugPrint", "incorrect Password");
     }
+    // --------------------------------
 
+    // Registration -------------------
+    // Go to Registration page
+    public void goToRegister(View view){ setContentView(R.layout.activity_register); }
+
+    // Save data to database
+    public void registerButton(View view){
+        Database database = new Database(getApplicationContext());
+        database.addUser(email.getText().toString(), password.getText().toString(), username.getText().toString());
+    }
+    // --------------------------------
 }
